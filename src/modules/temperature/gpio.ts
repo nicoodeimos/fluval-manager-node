e9cdb927f33669c6272bb5e0e653f89a7b69be75
Nicolas Bigot <nicolasbigot@icloud.com>
2017-02-27 22:36:35 +0100

    Update temperature command

--- src/modules/temperature/gpio.ts
+++ src/modules/temperature/gpio.ts
@@ -27,7 +27,7 @@ export class GPIO {
     }
 
     public readTemperature(): string {
-        let command = "find /sys/bus/w1/devices/ -name \"28-*\" -exec cat {}/w1_slave \; | grep \"t=\" | awk -F \"t=\" '{print $2/1000}'"
+        let command = "find /sys/bus/w1/devices/ -name \"28-*\" -exec cat {} /w1_slave ; | grep \"t=\" | awk -F \"t=\" '{print $2/1000}'"
         return this.execCommand(command);
     }
 
